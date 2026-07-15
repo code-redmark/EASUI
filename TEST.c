93203f88444264eec69035300c03386cfe53ee3f
NOOUBY-DEV <NOOUBY.DEV@gmail.com>
2026-07-16 03:56:04 +0800

    [&] fix : INDEX ISSUE IN GET_LAST_ELEMENT_INDEX() and ADD_ELEMENT()
    fixed the infinite 0 index bug

--- TEST.c
+++ TEST.c
@@ -8,6 +8,7 @@ int main()
 
         EASUI_WINDOW TEST_WINDOW;
         EASUI_LABEL TEST_LABEL;
+        EASUI_LABEL TEST_LABEL_2;
 
 
         EASUI_INIT();
@@ -19,14 +20,18 @@ int main()
                 SET_NEW_EASUI_WINDOW(&TEST_WINDOW, 16, 300, 200);
 
 
-                printf("ADDRESS OF WINDOW ELEMENT_LIST = %p\n", TEST_WINDOW.ELEMENT_LIST);
+                printf("ADDRESS OF WINDOW ELEMENT_LIST = %p\n\n", TEST_WINDOW.ELEMENT_LIST);
 
 
                 SET_NEW_EASUI_LABEL(&TEST_LABEL, &TEST_WINDOW, 0, 0, 20, 10, 12, 1024);
+                SET_NEW_EASUI_LABEL(&TEST_LABEL_2, &TEST_WINDOW, 0, 0, 20, 10, 12, 1024);
 
 
                 printf("ADDRESS OF TEST_LABEL = %p\n", &TEST_LABEL);
-                printf("ADDRESS OF WINDOW ELEMENT_LIST[0] (SHOULD BE TEST_LABEL) = %p\n", TEST_WINDOW.ELEMENT_LIST[0]);
+                printf("ADDRESS OF WINDOW ELEMENT_LIST[0] (SHOULD BE TEST_LABEL) = %p\n\n", (void*)TEST_WINDOW.ELEMENT_LIST[0]);
+
+                printf("ADDRESS OF TEST_LABEL_2 = %p\n", &TEST_LABEL_2);
+                printf("ADDRESS OF WINDOW ELEMENT_LIST[1] (SHOULD BE TEST_LABEL_2) = %p\n", TEST_WINDOW.ELEMENT_LIST[1]);
 
         }
 
