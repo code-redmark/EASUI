67a95c83f1db13a487d175fa6e93fdc627b60b3e
NOOUBY-DEV <NOOUBY.DEV@gmail.com>
2026-07-25 00:49:36 +0800

    [&] restage : missing EASUI.h changes

--- EASUI/EASUI.h
+++ EASUI/EASUI.h
@@ -2,6 +2,9 @@
 #ifdef EASUI
 
 
+
+
+
 #include "DEPENDENCIES/GLAD/include/glad/glad.h"
 #include "DEPENDENCIES/COMMON/STRINGS.h"
 #include <SDL3/SDL.h>
@@ -9,118 +12,134 @@
 #include <stdlib.h>
 
 
+
+
 #define EASUI_NONE 0
 #define EASUI_ERROR 0
 #define EASUI_OK 1
 
 
-typedef struct EASUI_BASE_ELEMENT EASUI_BASE_ELEMENT;
-typedef struct EASUI_SCREEN EASUI_SCREEN;
-typedef struct EASUI_WINDOW EASUI_WINDOW;
 
 
-
-
-typedef struct EASUI_VEC3
-{
-
-        float X;
-        float Y;
-        float Z;
-
-}
-EASUI_VEC3;
-
-
-typedef struct EASUI_VEC2
-{
-
-        float X;
-        float Y;
-
-}
-EASUI_VEC2;
-
-
-// ================================================== [FUNCTIONS] =================================================
+// [CUSTOM TYPES] =====================================================================================================================
 //
 
-int EASUI__INIT(const unsigned short MAX_WINDOW_COUNT);
-
-
-int EASUI__RUN();
-
-//
-// ================================================================================================================
-
-
-
-// ================================================= [BASE ELEMENT] =============================================
-//
-
-        struct EASUI_BASE_ELEMENT
+        typedef struct EASUI_VEC3
         {
 
-                void* INTERNAL_DATA;
+                float X;
+                float Y;
+                float Z;
 
-        };
-
-//
-// =============================================================================================================
+        }
+        EASUI_VEC3;
 
 
-// ================================================== [SCREEN] =================================================
-//
-
-        struct EASUI_SCREEN
+        typedef struct EASUI_VEC2
         {
 
-                void* INTERNAL_DATA;
-                int (*ADD_ELEMENT)(EASUI_SCREEN* SCREEN, void* ELEMENT);
+                float X;
+                float Y;
 
-        };
-
-
-        int SET_NEW_EASUI_SCREEN(EASUI_SCREEN* SCREEN, const unsigned short MAX_ELEMENT_COUNT);
+        }
+        EASUI_VEC2;
 
 //
-// =============================================================================================================
+// ====================================================================================================================================
 
 
-// ================================================== [WINDOW] =================================================
-
-        struct EASUI_WINDOW
-        {
-
-                void* INTERNAL_DATA;
-                int (*ADD_ELEMENT)(EASUI_WINDOW* WINDOW, void* ELEMENT);
-                int (*START)(EASUI_WINDOW* WINDOW);
-                EASUI_VEC3 BG_COLOR;
-        };
+// [FUNCTIONS] ========================================================================================================================
+//
+        int EASUI__INIT(const unsigned short MAX_WINDOW_COUNT);
 
 
-        int SET_NEW_EASUI_WINDOW(EASUI_WINDOW* WINDOW, const char* TITLE, const unsigned short MAX_ELEMENT_COUNT, const EASUI_VEC2 SIZE, const int RESIZABLE);
+        int EASUI__RUN();
 
-// =============================================================================================================
+//
+// ====================================================================================================================================
 
 
 
-// =================================================== [LABEL] =================================================
+// [ELEMENTS] =========================================================================================================================
+//
 
-        typedef struct EASUI_LABEL EASUI_LABEL;
+        // [TYPE DEFINITIONS] ======================================================================================
+        //
+                typedef struct EASUI_BASE_ELEMENT EASUI_BASE_ELEMENT;
+                typedef struct EASUI_WINDOW EASUI_WINDOW;
+                typedef struct EASUI_SCREEN EASUI_SCREEN;
+                typedef struct EASUI_LABEL EASUI_LABEL;
+
+        //
+        // ==========================================================================================================
 
 
-        struct EASUI_LABEL
-        {
+        // [BASE ELEMENT] ===========================================================================================
+        //
+                struct EASUI_BASE_ELEMENT
+                {
 
-                void* INTERNAL_DATA;
+                        void* INTERNAL_DATA;
 
-        };
+                };
+
+        //
+        // ==========================================================================================================
 
 
-        int SET_NEW_EASUI_LABEL(EASUI_LABEL* LABEL, void* OPTIONAL__FRAMED_HOLDER, const EASUI_VEC2 POSITION, const EASUI_VEC2 SIZE, const unsigned int FONT_SIZE, const unsigned long MAX_STRING_SIZE);
+        // [SCREEN] =================================================================================================
+        //
+                struct EASUI_SCREEN
+                {
 
-// ===============================================================================================================
+                        void* INTERNAL_DATA;
+                        int (*ADD_ELEMENT)(EASUI_SCREEN* SCREEN, void* ELEMENT);
+
+                };
+
+
+                int SET_NEW_EASUI_SCREEN(EASUI_SCREEN* SCREEN, const unsigned short MAX_ELEMENT_COUNT);
+
+        //
+        // =========================================================================================================
+
+
+        // [WINDOW] ================================================================================================
+        //
+                struct EASUI_WINDOW
+                {
+
+                        void* INTERNAL_DATA;
+                        int (*ADD_ELEMENT)(EASUI_WINDOW* WINDOW, void* ELEMENT);
+                        int (*START)(EASUI_WINDOW* WINDOW);
+                        EASUI_VEC3 BG_COLOR;
+                };
+
+
+                int SET_NEW_EASUI_WINDOW(EASUI_WINDOW* WINDOW, const char* TITLE, const unsigned short MAX_ELEMENT_COUNT, const EASUI_VEC2 SIZE, const int RESIZABLE);
+
+        //
+        // =========================================================================================================
+
+
+        // [LABEL] =================================================================================================
+        //
+                struct EASUI_LABEL
+                {
+
+                        void* INTERNAL_DATA;
+
+                };
+
+
+                int SET_NEW_EASUI_LABEL(EASUI_LABEL* LABEL, void* OPTIONAL__FRAMED_HOLDER, const EASUI_VEC2 POSITION, const EASUI_VEC2 SIZE, const unsigned int FONT_SIZE, const unsigned long MAX_STRING_SIZE);
+
+        //
+        // =========================================================================================================
+
+//
+// ====================================================================================================================================
+
 
 
 #endif
